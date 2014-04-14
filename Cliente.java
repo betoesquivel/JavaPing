095c1615985686a9197fa316e58bf39b9894e305
Beto Esquivel <ppbeto94@gmail.com>
2014-04-14 18:02:25 -0500

    Finished coding the client... next on is the server

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Cliente{
    public static void main(String args[]){
@@ -17,31 +18,45 @@ public class Cliente{
 	}
       try {
       String firstLine = "PING " + addr.getHostName() + "(" + addr.getHostAddress() + ")"
-          + "56(84) bytes of data.";
+          + " 56(84) bytes of data.";
       String templateAnswer = "64 bytes from " + addr.getHostName() + "(" + addr.getHostAddress()
           + "): icmp_req=";
-      String templateStatistics =
+      String templateStatistics = "--- " + addr.getHostName() + " ping statistics ---";
+      int transmitted = 0, received = 0;
+      double packetLoss = 0, min = 10000, avg = 0, max = 0;
 
       InetAddress server= InetAddress.getByName(hostname);
       BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
       DatagramSocket socket = new DatagramSocket();
       System.out.println(firstLine);
       int cont = 0;
-      double time = 0;
+      long time = 0;
+      double dTime = 0;
       while (cont < 4) {
         byte[] dataOut = new byte[templateAnswer.length()];
         dataOut = templateAnswer.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(dataOut, dataOut.length, server, 8888);
-        time = 0;
+        time = System.currentTimeMillis();
         socket.send(sendPacket);
+        transmitted++;
         byte[] dataIn = new byte[256];
         DatagramPacket receivePacket = new DatagramPacket(dataIn, 256);
         socket.receive (receivePacket);
-        time = 0;
+        if (receivePacket != null){
+            received++;
+            time = System.currentTimeMillis() - time;
+            dTime = time * .01;
+            min = (dTime < min) ? dTime : min;
+            max = (dTime > max) ? dTime : max;
+            avg += dTime;
+        }
         String s = new String(receivePacket.getData(), 0, receivePacket.getLength());
-        System.out.println(s + cont + " time=" + time +  " ms");
+        System.out.format(s + "%d time=%.2f ms%n", cont, time*.01);
         cont ++;
       }  // end while
+         System.out.println(templateStatistics);
+         System.out.format("%d transmitted, %d received, %d packet loss%n", transmitted, received, ( int ) (packetLoss * 100));
+         System.out.format("min/avg/max = %.3f/%.3f/%.3f ms%n", min, avg, max);
      }  // end try
      catch (UnknownHostException e) {System.err.println(e);}
      catch (SocketException se) {System.err.println(se);}
