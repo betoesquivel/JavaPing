e0a32b584a30af5dc0f873c3c3d9e19d4f6059cf
Beto Esquivel <ppbeto94@gmail.com>
2014-05-07 21:53:54 -0500

    possiblity of starting Servidor with percentage loss as an int parameter

--- Servidor.java
+++ Servidor.java
@@ -8,7 +8,28 @@ public class Servidor
    public static void main(String args[]){
 
 	byte[] buffer = new byte[4096];
-    int porcentajePerdida = Integer.parseInt(args[0]);
+    int porcentajePerdida = 0;
+    if (args[0]!=null){
+        porcentajePerdida = Integer.parseInt(args[0]);
+    }
+
+    int[] randomLossPool = new int[100];
+    //Fill array with 1s
+    for (int i = 0; i<100; i++){
+        randomLossPool[i] = 1;
+    }
+
+    //randomlly fill with 0 n places in array, n being the
+    //desired loss percentage
+    int randomIndex = 0;
+    Random randomFill = new Random();
+    while(porcentajePerdida > 0){
+        randomIndex = randomFill.nextInt(99);
+        if(randomLossPool[randomIndex] != 0){
+            randomLossPool[randomIndex] = 0;
+            porcentajePerdida--;
+        }
+    }
 
       try{
            socket = new DatagramSocket(8888);
@@ -22,7 +43,8 @@ public class Servidor
         Random randno = new Random();
         Thread.sleep(randno.nextInt(10) + 10);
         Random randResponse = new Random();
-        if (randResponse.nextInt(5) != 0){
+
+        if (randomLossPool[randResponse.nextInt(99)] == 1 ){
             socket.send(packet);
         }
 	   }catch (IOException ioe){
